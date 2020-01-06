5d26291b1e2d1688bb6ceaa66a55a62c57556fe0
ayushchoudhary1214 <ayushchoudhary26@gmail.com>
2020-01-06 12:44:46 +0000

    new file

--- firstgit/change.java
+++ firstgit/change.java
@@ -2,11 +2,10 @@ public class change
 {
 	public static void main(String args[])
 	{
-<<<<<<< HEAD
+
 		System.out.println("build the code");
-=======
+
 		System.out.println(" build automatically ");
->>>>>>> f5c6d169a87efc44adaefcc575917b94b8a9ff8b
 	}
 }
 
